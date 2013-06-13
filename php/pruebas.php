96873a13d3109f95139dc43f4db3e925808203ff
kaibalam <rvalenzuela@umg.edu.gt>
2013-06-13 08:34:27 -0600

    actualización estilo bitácora

---
+++ php/pruebas.php
@@ -0,0 +1,10 @@
+<?php
+			echo "Hello, ", "World!", "<br>"; //NO usa parentesis
+			echo ("It's such a perfect day! <br>"); //parentesis ok
+			print "Hello, ". "world!" . "<br>"; //concatenación
+			print "Hello to worl again! <br>"; //No necesita paréntesis
+			print ("It's such a perfect day! <br>"); //paréntesis ok
+
+			print "Today is ";
+			print date("D M j G:i:s T Y");
+?>
